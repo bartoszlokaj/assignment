7b0c27ae61be8f5460b6a568480b3fa56c3eb44e
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-05-21 16:21:38 +0200

    outputing data in html

--- js/index.js
+++ js/index.js
@@ -2,13 +2,28 @@ const url = "https://www.reddit.com/r/funny.json";
 const btn = document.querySelector(".btn");
 let myData = '';
 
+btn.addEventListener("click", getData());
 
-btn.addEventListener("click", function() {
+
+formatThisShit = (responseObject) => { 
+  
+  return `<ul>
+             <li>Title: ${responseObject.title}</li>
+            <li>Upvotes: ${responseObject.upvotes}</li>
+             <li>Downvotes: ${responseObject.downvotes}</li>
+             <li>Score: ${responseObject.score}</li>
+             <li>Comments: ${responseObject.num_comments}</li>
+             <li>Created: ${responseObject.created}</li>
+           </ul>`;
+};
+
+function getData(){
   fetch(url)
     .then(resp => resp.json())
     .then(resp => {
-      console.log(resp);
+
       myData = resp.data.children.map(child => {
+        
         return {
           
               title: child.data.title,
@@ -17,11 +32,14 @@ btn.addEventListener("click", function() {
               score: child.data.score,
               num_comments: child.data.num_comments,
               created: new Date(child.data.created * 1000)
-            
         };
       });
-      str = JSON.stringify(myData);
-      document.querySelector('.posts').innerHTML = str;
-      console.log(myData);
+
+      let html = myData.map(entry => { 
+          return formatThisShit(entry);
+      });
+
+       document.querySelector(".posts").innerHTML = html;
+
     });
-});
+};
